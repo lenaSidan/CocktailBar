fc6a16902938d89288f3dcc633d881918844f55d
Elena Sidanova <l.sidan@gmx.de>
2023-07-30 20:57:37 +0200

    add changes to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
